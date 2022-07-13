fd8ca601623e029ceff25dad415191dd75217878
macbook <egoremelit@gmail.com>
2022-07-14 00:05:36 +0400

    Добавил Broadcast CONNECTIVITY_ACTION

--- app/src/main/java/ru/GB/weathergb/MainActivity.kt
+++ app/src/main/java/ru/GB/weathergb/MainActivity.kt
@@ -1,6 +1,13 @@
 package ru.GB.weathergb
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.CONNECTIVITY_ACTION
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ru.GB.weathergb.databinding.ActivityMainBinding
 import ru.GB.weathergb.view.DetailsFragment
@@ -8,6 +15,7 @@ import ru.GB.weathergb.view.DetailsFragment
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +24,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportFragmentManager.beginTransaction().add(R.id.container, DetailsFragment()).commit()
+
+        initBroadcast()
+
     }
 
+    private fun initBroadcast() {
+//        TODO() удалить после сдачи дз
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val noConnectivity =
+                    intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)
+                if (noConnectivity) {
+                    onConnectionLost()
+                } else {
+                    onConnectionFound()
+                }
+            }
+        }
+        registerReceiver(receiver, IntentFilter(CONNECTIVITY_ACTION))
+    }
+
+
+    fun onConnectionLost() {
+        Toast.makeText(this, "Connection lost", Toast.LENGTH_LONG).show()
+    }
+
+    fun onConnectionFound() {
+        Toast.makeText(this, "Connection found", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
 }
