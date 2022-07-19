aec2ffd8f98a8f703a54b416435c284765b82307
macbook <egoremelit@gmail.com>
2022-07-19 20:39:20 +0400

    added okHttp

--- app/src/main/java/ru/GB/weathergb/model/WeatherLoader.kt
+++ app/src/main/java/ru/GB/weathergb/model/WeatherLoader.kt
@@ -2,24 +2,63 @@ package ru.GB.weathergb.model
 
 import android.util.Log
 import com.google.gson.Gson
+import okhttp3.*
+import okio.IOException
 import org.json.JSONException
 import ru.GB.weathergb.BuildConfig
 import ru.GB.weathergb.model.WeatherDTO.WeatherDTO
 import ru.GB.weathergb.utils.getLines
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.net.MalformedURLException
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
-import kotlin.reflect.typeOf
 
 object WeatherLoader {
 
     private const val WEATHER_API_KEY = "X-Yandex-API-Key"
+    private const val BASE_URL = "https://api.weather.yandex.ru/v2/informers"
+    private const val REQUEST_TYPE = "OkHttp"
 
     fun requestWeather(lat: Double, lon: Double, onResponse: (WeatherDTO?) -> Unit) {
-        val uri = URL("https://api.weather.yandex.ru/v2/informers?lat=${lat}&lon=${lon}")
-        var myConnection:HttpsURLConnection? = null
+
+        when (REQUEST_TYPE) {
+            "HttpsURLConnection" -> httpsURLConnectionRequest(lat, lon, onResponse)
+            "OkHttp" -> okHttpRequest(lat, lon, onResponse)
+            else -> Log.e("error request type", "don't selected type.")
+        }
+    }
+
+    private fun okHttpRequest(lat: Double, lon: Double, onResponse: (WeatherDTO?) -> Unit) {
+        val client = OkHttpClient()
+        val builder = Request.Builder()
+
+        builder.header(WEATHER_API_KEY, BuildConfig.WEATHER_API_KEY)
+        builder.url(BASE_URL + "?lat=${lat}&lon=${lon}")
+
+        val request = builder.build()
+        val cal = client.newCall(request)
+
+        cal.enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                onResponse(null)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val serverResponse = response.body.string()
+                if (response.isSuccessful && serverResponse.isNotEmpty()){
+                    onResponse(Gson().fromJson(serverResponse, WeatherDTO::class.java))
+                }
+                else onResponse(null)
+            }
+        })
+    }
+
+    private fun httpsURLConnectionRequest(
+        lat: Double,
+        lon: Double,
+        onResponse: (WeatherDTO?) -> Unit
+    ) {
+        val uri = URL(BASE_URL + "?lat=${lat}&lon=${lon}")
+        var myConnection: HttpsURLConnection? = null
 
         Thread {
             try {
@@ -33,21 +72,24 @@ object WeatherLoader {
             } catch (e: JSONException) {
                 Log.e("requestWeather Error JSONException", e.message.toString())
                 onResponse(null)
-            } catch (e: java.io.IOException){
+            } catch (e: java.io.IOException) {
                 Log.e("requestWeather Error IOException", e.message.toString())
                 onResponse(null)
-            }catch (e: MalformedURLException){
+            } catch (e: MalformedURLException) {
                 Log.e("requestWeather Error MalformedURLException", e.message.toString())
                 onResponse(null)
-            }catch (e: Exception){
-                Log.e("requestWeather Error Exception", e.message.toString()
-                        + " Необработан тип ошибки ${e::class.qualifiedName}")
+            } catch (e: Exception) {
+                Log.e(
+                    "requestWeather Error Exception", e.message.toString()
+                            + " Необработан тип ошибки ${e::class.qualifiedName}"
+                )
                 onResponse(null)
-            }
-            finally {
+            } finally {
                 myConnection?.disconnect()
             }
         }.start()
     }
 }
+
+
 
